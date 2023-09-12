2e235e6aa33630a611cf9177bc11ad4d2b7e3f1e
Coeliacjack <144786947+Coeliacjack@users.noreply.github.com>
2023-09-12 17:14:49 +0800

    Add files via upload

---
+++ Projet#1/Start_working.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//  you may need other standard header files
+
+
+//  CITS2002 Project 1 2023
+//  Student1:   23451626   DAVIN DO
+//  Student2:   STUDENT-NUMBER2   NAME-2
+
+
+//  myscheduler (v1.0)
+//  Compile with:  cc -std=c11 -Wall -Werror -o myscheduler myscheduler.c
+
+
+//  THESE CONSTANTS DEFINE THE MAXIMUM SIZE OF sysconfig AND command DETAILS
+//  THAT YOUR PROGRAM NEEDS TO SUPPORT.  YOU'LL REQUIRE THESE //  CONSTANTS
+//  WHEN DEFINING THE MAXIMUM SIZES OF ANY REQUIRED DATA STRUCTURES.
+
+#define MAX_DEVICES                     4
+#define MAX_DEVICE_NAME                 20
+#define MAX_COMMANDS                    10
+#define MAX_COMMAND_NAME                20
+#define MAX_SYSCALLS_PER_PROCESS        40
+#define MAX_RUNNING_PROCESSES           50
+
+//  NOTE THAT DEVICE DATA-TRANSFER-RATES ARE MEASURED IN BYTES/SECOND,
+//  THAT ALL TIMES ARE MEASURED IN MICROSECONDS (usecs),
+//  AND THAT THE TOTAL-PROCESS-COMPLETION-TIME WILL NOT EXCEED 2000 SECONDS
+//  (SO YOU CAN SAFELY USE 'STANDARD' 32-BIT ints TO STORE TIMES).
+
+#define DEFAULT_TIME_QUANTUM            100
+
+#define TIME_CONTEXT_SWITCH             5
+#define TIME_CORE_STATE_TRANSITIONS     10
+#define TIME_ACQUIRE_BUS                20
+
+
+//  ----------------------------------------------------------------------
+
+#define CHAR_COMMENT                    '#'
+
+#define MAX_LENGTH                      256 //ASDFASDFASDF
+
+
+void read_sysconfig(const char *filename, long *time_quantum)
+{
+    FILE *file = fopen(filename, "r");      //open file (fopen() mode)
+
+    if(file == NULL) {
+        printf("Cannot open '%s' \n", filename);        //if file does not exist, cannot open, etc
+        exit(EXIT_FAILURE);
+    }
+
+    char line[MAX_LENGTH];      //BUFFER
+
+    //parameters: from buffer, max number of characters to read, file pointer
+    while(fgets(line , MAX_LENGTH , file) != NULL){
+        /*char *split_lines = strtok(line, "\n\t\r"); //splits newline, tab and returns
+
+        if (split_lines[0] == '#' || " " == NULL) {
+            continue;   //skips comments
+        }
+        */
+       printf("%s\n", line);
+    }    
+    
+    fclose(file);
+}
+
+
+int main(int argc, char *argv[])
+{
+//  ENSURE THAT WE HAVE THE CORRECT NUMBER OF COMMAND-LINE ARGUMENTS
+    if(argc != 3) {
+        printf("Usage: %s sysconfig-file command-file\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    //struct Device devices[MAX_DEVICES];
+    const char *filename = argv[1]; //use argv[1] as filename (sysconfig)
+    long time_quantum = 0;
+
+
+
+
+
+//  READ THE SYSTEM CONFIGURATION FILE
+    read_sysconfig(filename, &time_quantum);
+}
---
+++ Projet#1/readfile.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define Buffer_size     128
+
+void line_spitter(char *str)
+{
+    char words[4][128];
+    char *p = strtok (str, "\t, ");
+    int i = 0;
+
+     while (p)
+    {
+        strcpy(words[i], p);
+        //printf("%s\n", words[i]);
+        p = strtok(NULL, "\t, ");
+        i++;
+        
+    }
+
+     for (i = 0; i < 4; ++i) 
+        {
+            printf("%s\n", words[i]);
+        }
+    
+
+}
+
+int main(int argc, char *argv[])
+{
+        if(argc != 2)
+        {
+            printf("Incorrect number of arguments entered");
+            exit(EXIT_FAILURE);
+        }
+
+    FILE *file;
+    int program_count = 0;
+    int syscall_count = 0;
+    char line[Buffer_size];
+
+
+    file = fopen(argv[1], "r");
+
+    if (file == NULL) {
+        printf("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    while (fgets(line, Buffer_size, file)) 
+    {
+         
+        
+        if (line[0] == '#')
+        {
+
+            printf("A line was skipped\n");
+            continue;
+        }
+
+        else if (line[0] == ' ')
+            {
+                syscall_count ++;
+                line[strlen(line)-1] = '\0';
+                line_spitter(line);
+            }
+        
+        else
+        {
+            program_count ++;
+            printf("%s\n", line);
+        }
+        
+        
+    }
+        
+
+
+
+}
